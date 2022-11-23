821bfac759624e6a0da2a7a2532405f014a9c53d
Faith <kigenfaith20@gmail.com>
2022-11-23 22:34:19 +0300

    review string lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+const currentUser = "Faith Kigen"
+
+
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage= `Welcome to Flatbook, ${currentUser}!` .toUpperCase()
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser .slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
+
+
+
+
+
+
+
+
+
+
+
